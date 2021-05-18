f5916b9e16f15dfe38212a2b791be9aead82b433
Robert Sterenchak <robertsterenchak@Roberts-MacBook-Pro.local>
2021-05-18 06:13:23 -1000

    Used rubocop -a to make some auto corrections.

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -2,23 +2,20 @@
 
 unsorted_array = [4, 3, 78, 2, 0, 2]
 
-
 def bubble_sort(unsorted_array)
-
   sorted_array = []
   index = 0
-
 
   # START METHOD
 
   until unsorted_array == []
 
     index = 0
-    
+
     while index < unsorted_array.length - 1
 
       if unsorted_array[index] > unsorted_array[index + 1]
-        
+
         temp = unsorted_array[index + 1]
         unsorted_array[index + 1] = unsorted_array[index]
         unsorted_array[index] = temp
@@ -34,7 +31,7 @@ def bubble_sort(unsorted_array)
     end
 
     sorted_array.push(unsorted_array[index])
-    unsorted_array.pop()
+    unsorted_array.pop
 
     p unsorted_array
     p sorted_array
@@ -44,9 +41,7 @@ def bubble_sort(unsorted_array)
   final_array = sorted_array.reverse
 
   puts "The following array is sorted #{final_array}"
-
 end
 # ENDS METHOD
-
 
 bubble_sort(unsorted_array)
